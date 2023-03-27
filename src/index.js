4468ab9a977038ae63e37823075efec32c87609c
Jacek Pasierb <jack_byk@o2.pl>
2023-03-27 13:54:41 +0200

    Deleted example.html and Added search.html and created form for input search and added style from index.js

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const boxInput = document.querySelector(".boxInput");
+boxInput.style.height = "60px";
+boxInput.style.backgroundColor = '#0066ff';
+boxInput.style.display = "flex";
+boxInput.style.alignItems = "center";
+boxInput.style.justifyContent = "center";
+
+const input = document.querySelector("input");
+input.style.borderRadius = "5px";
+input.style.border = "0";
+input.style.outline = "none";
+input.style.padding = "10px";
