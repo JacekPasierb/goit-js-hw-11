379ff904e713d3a93984f6f2b2bec4bacf289d46
JacekPasierb <113291374+JacekPasierb@users.noreply.github.com>
2023-03-27 11:55:30 +0000

    Deploying to gh-pages from @ JacekPasierb/goit-js-hw-11@4468ab9a977038ae63e37823075efec32c87609c 🚀

---
+++ index.a6f37c21.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".boxInput");e.style.height="60px",e.style.backgroundColor="#0066ff",e.style.display="flex",e.style.alignItems="center",e.style.justifyContent="center";const t=document.querySelector("input");t.style.borderRadius="5px",t.style.border="0",t.style.outline="none",t.style.padding="10px";
+//# sourceMappingURL=index.a6f37c21.js.map
