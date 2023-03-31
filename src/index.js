ce475d686177ab82cd1d21e2c7a3b4efe78467cb
Jacek Pasierb <jack_byk@o2.pl>
2023-04-01 00:38:06 +0200

    Created method async await

--- src/index.js
+++ src/index.js
@@ -1,33 +1,37 @@
 import Notiflix from 'notiflix';
-const boxInput = document.querySelector('.boxInput');
-const input = document.querySelector('input');
-const form = document.querySelector('.search-form');
+const boxInputDOM = document.querySelector('.boxInput');
+const inputDOM = document.querySelector('input');
+const formDOM = document.querySelector('.search-form');
 
-const searchImages = e => {
-    e.preventDefault();
-     let imageListHtml = '';
-  let wordKey = input.value.trim().split(' ').join('+');
-    let page = 1;
-    const limit = 40;
-    
-  fetch(
+let wordKey;
+const limit = 40;
+
+const fetchImages = async () => {
+  const response = await fetch(
     `https://pixabay.com/api/?key=34775826-8245aeb15fb52e6c04f01aeda&q=${wordKey}&image_type=photo&orientation=horizontal&safesearch=true&page=2&per_page=40`
-  )
-    .then(fetchJsonToJs)
-    .then(data => {
-      console.log('data', data); // data handling
+  );
+  const images = await response.json();
+  
+  return images;
+}
+const viewImages = async () => {
+  try {
+    const images = await fetchImages();
+    console.log("images",images);
+    //todo create image for users
+    let imageListHtml = "";
+        const totalPages = images.total / limit;
+    console.log(totalPages);
     
-        const totalPages = data.total / limit;
-        console.log(totalPages)
-      if (data.total == 0) {
-        Notiflix.Notify.info(
-          'Sorry, there are no images matching your search query. Please try again.'
-        );
-      } else {
-        // console.log('1', data);
-       
-        data.hits.forEach(e => {
-          imageListHtml += `<div class="photo-card">
+    if (images.total == 0) {
+      Notiflix.Notify.info(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    } else {
+      // console.log('1', data);
+
+      images.hits.forEach(e => {
+        imageListHtml += `<div class="photo-card">
               <img src="${e.webformatURL}" alt="${e.tags}" loading="lazy" />
               <div class="info">
                 <p class="info-item">
@@ -44,21 +48,36 @@ const searchImages = e => {
                 </p>
               </div>
             </div>`;
-        }); 
-        //   console.log('2', imageListHtml);
-         
-        //   console.log('3', galery);
-          
-        }
-         const galery = document.querySelector('.gallery');
-      galery.innerHTML = imageListHtml;
-    })
-    .catch(error => {
-      console.log(error); // Error handling
-    });
-};
+      });
+  
+    }
+    const galery = document.querySelector('.gallery');
+    galery.innerHTML = imageListHtml;
+  } catch (error) {
+    console.log(error.message);
+  }
+}
 
-form.addEventListener('submit', searchImages);
+const searchImages = e => {
+    e.preventDefault();
+     let imageListHtml = '';
+    wordKey = inputDOM.value.trim().split(' ').join('+');
+    let page = 1;
+    
+  fetchImages(wordKey);
+ 
+  viewImages();
+    
+     
+    
+      
+         
+    }
+    
+    
+
+
+formDOM.addEventListener('submit', searchImages);
 
 const fetchJsonToJs = response => {
   if (!response.ok) {
